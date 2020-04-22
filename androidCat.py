e2ee10a0a236cfef7b4fa8da439ddf9533c6945b
YoCheng <xyox860224@gmail.com>
2020-04-22 13:46:26 +0800

    Update androidCat.py

--- androidCat.py
+++ androidCat.py
@@ -1,1 +1,12 @@
-# TODO
+from subprocess import Popen, PIPE
+
+while(True):
+    with Popen(['adb shell cat /storage/emulated/0/sensor.txt'], shell=True, stdout=PIPE) as proc:
+        string = proc.stdout.readline().decode('UTF-8')
+        try:
+            speed, zaxis = string.split(' ')
+            print("======================")
+            print("Speed: ", "{: .4f}".format(float(speed)), " m/s")
+            print("Zaxis: ", "{: .4f}".format(float(zaxis)), " m/s")
+        except Exception as identifier:
+            print("No value")
